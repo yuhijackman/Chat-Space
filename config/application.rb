a3f117801f78e9951c1b67c98c7e0c1c4f358c20
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-02-25 17:04:11 +0900

    resolve_conflict

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-
     config.generators do |g|
       g.assets     false
       g.helper     false
     end
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
