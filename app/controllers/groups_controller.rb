e2e62c2ea01d901dbb9951af2f3d649a068903f3
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-01 14:21:29 +0900

    Simplify_the_way_of_writing

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,18 +4,16 @@ class GroupsController < ApplicationController
   end
 
   def create
-      @group= Group.create(group_params)
-      if @group.save
-        redirect_to controller: :chats, action: :index, notice: 'チャットグループを作成しました'
-      else
-        redirect_to action: :new, alert: 'グループ名を入力してください'
-      end
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, action: :index, notice: 'チャットグループを作成しました'
+    else
+      redirect_to new_group_path, alert: 'グループ名を入力してください'
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name)
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
-  resources :groups, only: [:new,:create]
+  resources :groups, only: [:new, :create]
 end
