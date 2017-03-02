fee91d2d2cf4cc2ee59e8d9311a37b94420cafa1
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-02 14:52:32 +0900

    Integrate_Chatting_Function

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,17 @@
 class ChatsController < ApplicationController
+
   def index
   end
+
+  def create
+    Chat.create(chat_params)
+    redirect_to group_chats_path
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
-  resources :groups, except: [:index, :destroy]
+  resources :groups, except: [:index, :destroy] do
+    resources :chats, only: [:create, :index]
+  end
 end
