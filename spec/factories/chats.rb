08d7f4749303aba554bf0cd9525012f9834e9dc7
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-04 13:36:29 +0900

    Generate dummy data

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
 
-  factory :chat do
-    body              "Whatzaap"
-    image             ""
+  factory :chat do |f|
+    f.body Faker::Lorem.paragraph
     user_id           1
     group_id          1
   end
