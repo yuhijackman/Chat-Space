2a6920939f86c55875a19bef4ecc68d8cc02a328
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-04 12:46:52 +0900

    Resolve_Translation_Missing

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 describe Chat do
   describe '#create' do
+    it "is valid with a body" do
+      chat = build(:chat)
+      expect(chat).to be_valid
+    end
+
     it "is invalid without a body" do
+      chat = build(:chat, body: nil)
+      chat.valid?
+      expect(chat.errors[:body]).to include('が入力されていません')
     end
   end
 end
