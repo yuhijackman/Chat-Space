f13c240c8ea17124885dda203857e7a56963446a
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-14 19:01:33 +0900

    Introduce S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
 
   storage :fog
-
-  process convert: 'jpg'
 
   # 保存するディレクトリ名
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
   # thumb バージョン(width 400px x height 200px)
   version :thumb do
     process resize_to_fit: [400, 200]
