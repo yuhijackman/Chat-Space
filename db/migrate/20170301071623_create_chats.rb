c0ebf0ed1fdf5a647d6b3f81e8f6c79b67120dab
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-16 21:23:01 +0900

    Alter_description

--- db/migrate/20170301071623_create_chats.rb
+++ db/migrate/20170301071623_create_chats.rb
@@ -2,7 +2,7 @@ class CreateChats < ActiveRecord::Migration[5.0]
   def change
     create_table :chats do |t|
       t.string :body, null: false
-      t.text :image
+      t.string :image
       t.references :user, foreign_key: true
       t.references :group, foreign_key: true
       t.timestamps null: false
--- db/migrate/20170314073509_change_image_to_chats.rb
+++
@@ -1,9 +0,0 @@
-class ChangeImageToChats < ActiveRecord::Migration[5.0]
-  def up
-    change_column :Chats, :image, :string
-  end
-
-  def down
-    change_column :Chats, :image, :text
-  end
-end
